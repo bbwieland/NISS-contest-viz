aa2ba649f5c33ff5272d6961598300bda035b59f
bbwieland <benbowenwieland@gmail.com>
2022-03-14 20:10:09 -0400

    app v1 deployment

--- app/NISS-data-viz/app.R
+++ app/NISS-data-viz/app.R
@@ -2,9 +2,18 @@ library(shiny)
 library(tidyverse)
 library(maps)
 library(RColorBrewer)
+library(gt)
 
-data = read.csv("data/NCES.csv") %>%
-  mutate(State = gsub('.{1}$', '', State))
+div100 = function(x){
+  y = x/100
+  return(y)
+}
+
+data = read.csv(
+  "https://raw.githubusercontent.com/bbwieland/NISS-contest-viz/main/data/NCES.csv"
+) %>%
+  mutate(State = gsub('.{1}$', '', State)) %>%
+  mutate_if(is.numeric,div100)
 dataStates = data %>% filter(State != "United States" &
                                State != "District of Columbia")
 dataHS = dataStates %>% filter(Degree == "High School")
@@ -38,45 +47,105 @@ ui <- fluidPage(
                    "Select a degree type to view:",
                    choices = c("High School", "Bachelors"),
                    selected = "High School"
-                 )
+                 ),
+                 sliderInput(
+                   "maprange",
+                   "Select a percent range for graph fill:",
+                   min = 0, max = 1,
+                   value = c(0.5,1))
+                 
                ),
-               mainPanel(
-                 width = 8,
-                 plotOutput("raceplot")
+               mainPanel(width = 8,
+                         fluidRow(column(
+                           width = 12, plotOutput("raceplot")
+                         )),
+                         fluidRow(
+                           column(width = 6, gt_output("racetabletop")),
+                           column(width = 6, gt_output("racetablebottom"))
+                         ))
+             ))
+    
+  ),
+  tabPanel(title = "Data"))
 )
-             )
-             
-    ),
-    tabPanel(title = "Data"))
-  )
-)
+
 # Define server logic
 server <- function(input, output) {
   plotdata = reactive({
     usMapTotal %>% filter(Degree == input$degreetype)
   })
   
+  tabledata = reactive({
+    dataStates %>% filter(Degree == input$degreetype &
+                            is.na(get(input$race)) == F) %>%
+      arrange(-get(input$race))
+  })
+  
+  tabletop = reactive({
+    head(tabledata(), 5) %>% select(State, Overall, !!(input$race)) %>%
+      gt() %>%
+      tab_header(title = md("**Five Highest Rates**")) %>%
+      fmt_percent(columns = c(2,3),decimals = 1) %>%
+      cols_align(columns = everything(),align = "c") %>%
+      opt_row_striping()
+    
+  })
+  
+  tablebottom = reactive({
+    tail(tabledata(), 5) %>% select(State, Overall, !!(input$race)) %>%
+      arrange(get(input$race)) %>%
+      gt() %>%
+      tab_header(title = md("**Five Lowest Rates**")) %>%
+      fmt_percent(columns = c(2,3),decimals = 1) %>%
+      cols_align(columns = everything(),align = "c") %>%
+      opt_row_striping()
+    
+    
+  })
+  
+
+  raceplot = reactive({ggplot() +
+    geom_polygon(
+      data = plotdata(),
+      aes_string(
+        x = "long",
+        y = "lat",
+        group = "group",
+        fill = input$race
+      ),
+      color = "black"
+    ) +
+    scale_fill_distiller(
+      palette = "RdYlGn",
+      direction = 1,
+      na.value = "lightgrey",
+      name = paste0(
+        "Percentage of\n",
+        input$race,
+        " Students \nWho Earned\n",
+        input$degreetype,
+        " Degree\n"
+      ),
+      limits = input$maprange,
+      labels = scales::percent
+    ) +
+    theme_void() +
+    theme(aspect.ratio = 2 / 3,
+          plot.title = element_text(face = "bold",size = 20),
+          plot.subtitle = element_text(face = "italic",size = 16)) +
+    labs(
+      title = paste(input$degreetype,"Degrees for",input$race,"Students"),
+      subtitle = "Sufficient data not available for states in gray"
+    )})
+  
   output$raceplot = renderPlot(
-    ggplot() +
-      geom_polygon(
-        data = plotdata(),
-        aes_string(
-          x = "long",
-          y = "lat",
-          group = "group",
-          fill = input$race
-        ),
-        color = "black"
-      ) +
-      scale_fill_distiller(
-        palette = "RdYlGn",
-        direction = 1,
-        na.value = "lightgrey",
-        name = paste0("Percentage of\n",input$race," Students \nWho Earned\n",input$degreetype," Degree")
-      ) +
-      theme_void() +
-      theme(aspect.ratio = 2/3)
+    raceplot()
   )
+  
+  output$racetabletop = render_gt(expr = tabletop())
+  output$racetablebottom = render_gt(expr = tablebottom())
+  
+
 }
 
 # Run the application
